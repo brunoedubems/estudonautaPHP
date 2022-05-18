d77414412c211edbcee1dcb8a694b99b279bc5f4
unknown <bemds1@aluno.ifnmg.edu.br>
2022-05-18 16:38:22 -0300

    Update banco.php

--- exemplo1/includes/banco.php
+++ exemplo1/includes/banco.php
@@ -1,3 +1,20 @@
 <?php
+$banco = new mysqli("localhost", "root", "", "bd_games");
+if($banco->connect_errno){
+echo "<p>Encontrei um erro $banco->errno --> $banco->connect_error </p>";
+die();
+}
 
-$banco = new mysqli();
+$banco->query("SET NAMES 'utf8'");
+$banco->query("SET character_set_connection = utf8");
+$banco->query("SET character_set_client= utf8");
+$banco->query("SET character_set_results= utf8");
+
+
+$busca = $banco->query("select * from generos");
+if(!$busca){
+    echo "<p>Falha na Busca! $banco->connect_error</p>";
+} else{
+    $reg = $busca->fetch_object();
+    print_r($reg);
+}
